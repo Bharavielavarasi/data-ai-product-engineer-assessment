0fad66bedad72345fa752d8bc3b8c0af39a53e8e
Bharavielavarasi <117898568+Bharavielavarasi@users.noreply.github.com>
2026-05-29 12:51:39 +0530

    Added sample aggregation query

--- task-2-pipeline/query.sql
+++ task-2-pipeline/query.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    temperature_category,
+    AVG(max_temperature) AS avg_max_temperature,
+    AVG(min_temperature) AS avg_min_temperature
+FROM weather_dataset.weather_data
+GROUP BY temperature_category
+ORDER BY avg_max_temperature DESC;
